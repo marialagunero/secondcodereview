8afc385579614f69ff3c75694b45f0e88a326dd9
Maria Lagunero <maria58@gmail.com>
2019-10-25 10:48:17 -0700

    add js and functions to questions and got the click results to work

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+  $("form#Survey").submit(function(event) {
+    event.preventDefault();
+    var name = $("input#name").val();
+    $(".outputName").text(name);
+
+    var animal = $("input:radio[name=animal]:checked").val();
+    $(".outputAnimal").text(animal);
+
+    var color = $("input:radio[name=color]:checked").val();
+    $(".outputColor").text(color);
+
+    var drink = $("input:radio[name=drink]:checked").val();
+    $(".outputDrink").text(drink);
+
+    var hobby = $("input:radio[name=hobby]:checked").val();
+    $(".outputHobby").text(hobby);
+
+    var flavor = $("input:radio[name=flavor]:checked").val();
+    $(".outputFlavor").text(flavor);
+
+
+
+    $(".results").show();
+  });
+
+});
