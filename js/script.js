d31164f929dad4cc5ab471cd3347577b13428dd4
Maria Lagunero <maria58@gmail.com>
2019-10-25 13:37:03 -0700

    add continued attempts to make js/jquery side work.

--- js/script.js
+++ js/script.js
@@ -26,21 +26,3 @@ $(document).ready(function() {
   });
 
 });
-
-
-$("#heightchart").submit(function(event) {
-  var userHeight = parseInt($("#height").val());
-  event.preventDefault();
-  $(".hidden").hide();
-
-  if (userHeight >= 6){
-    $("#YouSure").show();
-
-  } else if
-  (userHeight <= 5 && userHeight>=4){
-
-    $("#Yikes, #EzMode").show();
-  } else (userHeight <= 3)
-    $("#EzMode").show();
-  });
-});
