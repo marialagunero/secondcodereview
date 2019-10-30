b42e18910f3bb39c48c9b5d96d8594140a7f1203
Maria Lagunero <maria58@gmail.com>
2019-10-29 20:35:07 -0700

    add tried to add functionality to js. Still working

--- js/script.js
+++ js/script.js
@@ -5,24 +5,44 @@ $(document).ready(function() {
     var name = $("input#name").val();
     $(".outputName").text(name);
 
-    var animal = $("input:radio[name=animal]:checked").val();
-    $(".outputAnimal").text(animal);
+    // var animal = $("input:radio[name=animal]:checked").val();
+    // $(".outputAnimal").text(animal);
+    //
+    // var color = $("input:radio[name=color]:checked").val();
+    // $(".outputColor").text(color);
+    //
+    // var drink = $("input:radio[name=drink]:checked").val();
+    // $(".outputDrink").text(drink);
+    //
+    // var hobby = $("input:radio[name=hobby]:checked").val();
+    // $(".outputHobby").text(hobby);
+    //
+    // var flavor = $("input:radio[name=flavor]:checked").val();
+    // $(".outputFlavor").text(flavor);
 
-    var color = $("input:radio[name=color]:checked").val();
-    $(".outputColor").text(color);
+    var animal = $("#animal").val();
+    var color = $("#color)").val();
+    var drink = $("#drink").val();
+    var hobby = $("#hobby").val();
+    var flavor =$("#flavor").val();
 
-    var drink = $("input:radio[name=drink]:checked").val();
-    $(".outputDrink").text(drink);
+    if (animal === "dog") {
+      $("#ruby").show();
+      return
+    }
+    if (color === "blue") {
+      $("#javascript").show();
+      return
+    }
+    if (drink === "beer") {
+      $("#python").show();
+      return
+    };
 
-    var hobby = $("input:radio[name=hobby]:checked").val();
-    $(".outputHobby").text(hobby);
-
-    var flavor = $("input:radio[name=flavor]:checked").val();
-    $(".outputFlavor").text(flavor);
 
 
 
-    $(".results").show();
+    // $(".results").show();
   });
 
 });
